38e7fdd964e97208c82f3fe899754c7f03962097
Gordon Chen <Gordon Chen>
2017-08-15 23:38:41 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { MdInputModule, MdErrorDirective, MdButtonModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { firebaseInfo } from '../environments/firebase-info';
 
 import { AppComponent } from './app.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { ComponentComponent } from './component/component.component';
+
+import { AuthService } from './providers/auth.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelsComponent,
+    ComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MdInputModule,
+    MdButtonModule,
+    AngularFireModule.initializeApp(firebaseInfo.firebase, 'chat-app'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [MdErrorDirective, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/providers/auth.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class AuthService {
+
+	authState: any = null;
+
+	constructor(public afAuth: AngularFireAuth, private db: AngularFireDatabase) { 
+		this.afAuth.authState.subscribe(auth => {
+			this.authState = auth;
+		})
+	}
+
+	get authenticated(): boolean {
+		return this.authState !== null;
+	}
+
+	get currentUser(): any {
+		return this.authenticated ? this.authState: null;
+	}
+
+	get currentUserObservable(): any {
+		return this.afAuth.authState;
+	}
+
+	get currentUserId(): string {
+		return this.authenticated ? this.authState.uid: '';
+	}
+
+	get currentUserAnonymous(): boolean {
+		return this.authenticated ? this.authState.isAnonymous: false;
+	}
+
+	get currentUserDisplayName(): string {
+		if(!this.authState) { return 'Guest' }
+		else if(this.currentUserAnonymous) { return 'Anonymous' }
+		else { return this.authState['displayName'] || 'User without a Name' }
+	}
+
+	createNewUser(email: string, password: string){
+		this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+		.then(user => {
+			this.authState = user;
+			this.updateUserData();
+		})
+		.catch(error => console.log(error))
+	}
+
+	signInWithEmail(email: string, password: string){
+		this.afAuth.auth.signInWithEmailAndPassword(email, password)
+		.then(user => {
+			this.authState = user;
+			this.updateUserData();
+		})
+		.catch(error => console.log(error))
+	}
+
+	oAuthSignIn(provider){
+		this.afAuth.auth.signInWithRedirect(provider)
+			.then(credentials => {
+				this.authState = credentials.user;
+				this.updateUserData();
+			})
+			.catch(error => console.log(error))
+	}
+
+	googleLogin(){
+		const provider = new firebase.auth.GoogleAuthProvider();
+		return this.oAuthSignIn(provider)
+	}
+
+	//send password reset email
+	resetPassword(email: string) {
+		var auth = firebase.auth();
+		return auth.sendPasswordResetEmail(email)
+			.then(() => console.log("email sent"))
+			.catch(error => console.log(error))
+	}
+
+	signOut(): void {
+		this.afAuth.auth.signOut();
+	}
+
+	private updateUserData(): void {
+		let path = 'users/${this.currentUserId}';
+		let data = {
+			email: this.authState.email,
+			name: this.authState.displayName
+		}
+		this.db.object(path).update(data)
+			.catch(error => console.log(error))
+	}
+}
